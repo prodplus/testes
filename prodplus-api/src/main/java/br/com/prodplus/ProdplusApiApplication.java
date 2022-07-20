4811fd2a05a29f3a7526fd4137989a9fad4eec68
Marlon F Garcia <marlon_garcia@outlook.com>
2022-07-20 10:47:15 -0300

    Projeto API criado

---
+++ prodplus-api/src/main/java/br/com/prodplus/ProdplusApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.prodplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdplusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdplusApiApplication.class, args);
+	}
+
+}
---
+++ prodplus-api/src/test/java/br/com/prodplus/ProdplusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prodplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdplusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
